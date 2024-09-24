2a8f02bc00c5ada1d63e840b62218c851c5bf0da
emii2007 <00001129305090sp@al.educacao.sp.gov.br>
2024-09-24 17:57:41 -0300

    Primeira vesão do código

---
+++ script.js
@@ -0,0 +1,13 @@
+
+/*Emily Crislaine 3C 20.09*/
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
